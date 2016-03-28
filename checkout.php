e04645186fac4d512974f8790c3c7c370a242d0f
ashiramin <ashir.amin@uky.edu>
2016-03-28 11:31:08 -0400

    update inventory working

---
+++ checkout.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ashir
+ * Date: 3/28/16
+ * Time: 11:29 AM
+ */
+
+?>
+
+
--- inventory/index.php
+++ inventory/index.php
@@ -11,32 +11,92 @@
 require '../config/conn.php';
 require '../include/header.php';
 
+$SQL = $conn->prepare("Select * from Product");
+
+$SQL->execute();
 
 ?>
 
-<div class="well bs-component col-lg-4 col-lg-offset-4">
-    <form class="form-horizontal" action="process/processRegister.php" method="post">
-        <legend>Sign Up</legend>
-        <fieldset>
-            <div class="form-group col-md-10">
-                <input type="text" placeholder="Username" name="username" id="username" class="form-control">
-            </div>
-            <div class="form-group col-md-10">
-                <input type="password" name="password" placeholder="Password" class="form-control">
-            </div>
-            <div class="form-group col-md-10">
-                <input type="password" name="confirmpassword" placeholder="Password" class="form-control">
-            </div>
-            <div class="form-group">
-                <div class="col-lg-10">
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                </div>
-            </div>
-        </fieldset>
-    </form>
+<table class="table table-striped table-bordered">
+    <thead>
+    <tr>
+        <th>Name</th>
+        <th>Price</th>
+        <th>Quantity</th>
+        <th>Action</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php
+        while ($info = $SQL->fetch()) {
+            ?>
+            <tr>
+                <td><?=$info["Name"]?></td>
+                <td><?=$info["Price"]?></td>
+                <td>
+                    <span><?=$info["Qty"]?></span>
+                    <input type="number" min="1" value="<?=$info["Qty"]?>"  style="display: none;" class="form-control" />
+                    <input type="hidden" value="<?=$info["Id"]?>" />
+                </td>
+                <td>
+                    <button  class="btn btn-success edit">Edit</button>
 
-</div>
+                </td>
+            </tr>
+            <?
+        }
+    ?>
+
+    </tbody>
+</table>
 
 <?php
 require '../include/footer.php';
 ?>
+
+<script>
+
+
+    $(document).ready(function(){
+        $('tr td:nth-child(3)').click(function() {
+            console.log("asds");
+            console.log($(this));
+        });
+
+        $('.edit').click(function () {
+            var $input =  $(this).parent().parent().find("input[type=number]");
+            var $span =  $(this).parent().parent().find("span");
+
+            if ($input.is(":visible")) {
+                $(this).text("Edit");
+                $span.text($input.val());
+                $span.show();
+                $input.hide();
+                $.post("updateInventory.php",
+                    {Id:$(this).parent().parent().find("input[type=hidden]").val(),Qty :$input.val() });
+
+            }
+            else {
+                $(this).text("Save");
+                $input.show();
+                $input.focus();
+                $span.hide();
+
+            }
+
+
+
+            console.log($span.is(":visible"));
+            console.log($input.is(":visible"));
+
+        });
+
+
+
+
+
+    });
+
+
+</script>
+
